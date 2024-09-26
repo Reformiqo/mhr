a0ca58228ba3195c7e39120cd37e252a3d250a70
Momodou Khan <khanmomodou101@gmail.com>
2024-09-26 03:00:53 +0000

    update

--- mhr/batch_qr_code.py
+++ mhr/batch_qr_code.py
@@ -1,31 +1,32 @@
 import frappe
-import segno
+# import segno
 @frappe.whitelist()
 def set_si_qrcode(doc, method=None):
+    pass
     
-    img = segno.make_qr("Hello, World")
+    # img = segno.make_qr("Hello, World")
     
-    # Define the file path
-    file_path = f"/home/frappe/frappe-bench/sites/sona.erpera.io/public/files/{doc.name}.png"
+    # # Define the file path
+    # file_path = f"/home/frappe/frappe-bench/sites/sona.erpera.io/public/files/{doc.name}.png"
     
-    # Save the image to the file
-    img.save(file_path, scale=50)  
+    # # Save the image to the file
+    # img.save(file_path, scale=50)  
 
-    # Read the image file from the system
-    with open(file_path, "rb") as file:
-        file_content = file.read()
+    # # Read the image file from the system
+    # with open(file_path, "rb") as file:
+    #     file_content = file.read()
 
 
-    # Create a new file document in Frappe
-    file_doc = frappe.get_doc({
-        "doctype": "File",
-        "file_name": f"{doc.name}.png",
-        "is_private": 0,  # 0 for public, 1 for private
-        "content": file_content,
+    # # Create a new file document in Frappe
+    # file_doc = frappe.get_doc({
+    #     "doctype": "File",
+    #     "file_name": f"{doc.name}.png",
+    #     "is_private": 0,  # 0 for public, 1 for private
+    #     "content": file_content,
         
-    })
+    # })
 
-    file_doc.insert()
-    frappe.db.set_value("Batch", doc.name, "custom_qr_image", file_doc.file_url)
-    frappe.db.commit()
-    return file_doc.file_url
+    # file_doc.insert()
+    # frappe.db.set_value("Batch", doc.name, "custom_qr_image", file_doc.file_url)
+    # frappe.db.commit()
+    # return file_doc.file_url
