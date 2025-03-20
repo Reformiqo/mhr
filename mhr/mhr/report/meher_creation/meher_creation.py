2f43fd0ff22d2933873857fd861bd2128f9899b5
Momodou khan <khanmomodou101@gmail.com>
2025-03-20 06:59:37 +0000

    update

--- mhr/mhr/report/meher_creation/meher_creation.py
+++ mhr/mhr/report/meher_creation/meher_creation.py
@@ -2,116 +2,65 @@ import frappe
 from frappe import _
 from frappe.utils import cint
 
-
 def execute(filters=None):
-    columns, data = get_columns(filters), get_data(filters)
+    columns, data = get_columns(filters=filters), get_datas(filters=filters)
     return columns, data
-
 
 def get_columns(filters=None):
     return [
         {"label": _("Date"), "fieldname": "date", "fieldtype": "Date", "width": 150},
-        {
-            "label": _("Container"),
-            "fieldname": "container",
-            "fieldtype": "Data",
-            "width": 150,
-        },
-        {
-            "label": _("Product Name"),
-            "fieldname": "item",
-            "fieldtype": "Data",
-            "width": 100,
-        },
+        {"label": _("Container"), "fieldname": "container", "fieldtype": "Data", "width": 150},
+        {"label": _("Product Name"), "fieldname": "item", "fieldtype": "Data", "width": 100},
         {"label": _("Pulp"), "fieldname": "pulp", "fieldtype": "Data", "width": 100},
-        {
-            "label": _("Lusture"),
-            "fieldname": "lusture",
-            "fieldtype": "Data",
-            "width": 100,
-        },
+        {"label": _("Lusture"), "fieldname": "lusture", "fieldtype": "Data", "width": 100},
         {"label": _("Glue"), "fieldname": "glue", "fieldtype": "Data", "width": 100},
-        {
-            "label": _("Grade"),
-            "fieldname": "grade",
-            "fieldtype": "Data",
-            "width": 100,
-        },
-        {
-            "label": _("Total Closing"),
-            "fieldname": "total_closing",
-            "fieldtype": "Int",
-            "width": 100,
-        },
-        {
-            "label": _("Mer No"),
-            "fieldname": "mer_no",
-            "fieldtype": "Data",
-            "width": 100,
-        },
-        {
-            "label": _("Lot No"),
-            "fieldname": "lot_no",
-            "fieldtype": "Data",
-            "width": 100,
-        },
+        {"label": _("Total Closing"), "fieldname": "total_closing", "fieldtype": "Data", "width": 100},
+        {"label": _("Grade"), "fieldname": "grade", "fieldtype": "Data", "width": 100},
+        {"label": _("Mer No"), "fieldname": "mer_no", "fieldtype": "Data", "width": 100},
+        {"label": _("Lot No"), "fieldname": "lot_no", "fieldtype": "Data", "width": 100},
         {"label": _("Cone"), "fieldname": "cone", "fieldtype": "Data", "width": 100},
-        {"label": _("Boxes"), "fieldname": "boxes", "fieldtype": "Int", "width": 100},
-        {"label": _("Stock"), "fieldname": "stock", "fieldtype": "Int", "width": 100},
+        {"label": _("Boxes"), "fieldname": "boxes", "fieldtype": "Data", "width": 100},
+        {"label": _("Stock"), "fieldname": "stock", "fieldtype": "Data", "width": 100},
+        
     ]
 
+def get_datas(filters=None):
+    containers = frappe.get_all("Container", fields=["*"], filters=filters)
+    data = []
 
-def get_data(filters=None):
-    conditions = get_conditions(filters)
+    for container in containers:
+        cones = get_multiple_variable_of_cone(container.name)
+        for cone in cones:
+            data.append({
+                "date": container.posting_date,
+                "container": container.container_no,
+                "item": container.item,
+                "pulp": container.pulp,
+                "lusture": container.lusture,
+                "glue": container.glue,
+                "total_closing": get_total_closing(container.name),
+                "mer_no": container.merge_no,
+                "lot_no": container.lot_no,
+                "grade": container.grade,
+                "cone": cone,
+                "boxes": get_number_of_boes(container.name, cone),
+                "stock": get_cone_total(container.name, cone)
+            })
+        
+    return data
 
-    query = """
-        SELECT 
-            c.posting_date as date,
-            c.container_no as container,
-            c.item,
-            c.pulp,
-            c.lusture,
-            c.glue,
-            c.grade,
-            c.merge_no as mer_no,
-            c.lot_no,
-            cb.cone,
-            COUNT(cb.name) as boxes,
-            SUM(cb.qty) as total_closing,
-            SUM(CASE WHEN cb.cone = cb.cone THEN cb.cone ELSE 0 END) as stock
-        FROM 
-            `tabContainer` c
-        LEFT JOIN 
-            `tabContainer Batch` cb ON c.name = cb.parent
-        WHERE 
-            1=1
-            {conditions}
-        GROUP BY 
-            c.name, cb.cone
-        ORDER BY 
-            c.posting_date DESC, c.container_no, cb.cone
-    """.format(
-        conditions=conditions
-    )
+def get_multiple_variable_of_cone(container_name):
+    con = frappe.get_doc("Container", container_name)
+    return list(set(batch.cone for batch in con.batches))
 
-    return frappe.db.sql(query, filters, as_dict=1)
+def get_cone_total(container_name, cone):
+    con = frappe.get_doc("Container", container_name)
+    return sum(cint(batch.cone) for batch in con.batches if batch.cone == cone)
 
+def get_total_closing(container_name):
+    con = frappe.get_doc("Container", container_name)
+    return sum(cint(batch.qty) for batch in con.batches)
 
-def get_conditions(filters):
-    conditions = []
-
-    if filters:
-        if filters.get("posting_date"):
-            conditions.append("c.posting_date = %(posting_date)s")
-        if filters.get("container_no"):
-            conditions.append("c.container_no = %(container_no)s")
-        if filters.get("item"):
-            conditions.append("c.item = %(item)s")
-        if filters.get("merge_no"):
-            conditions.append("c.merge_no = %(merge_no)s")
-        if filters.get("lot_no"):
-            conditions.append("c.lot_no = %(lot_no)s")
-        if filters.get("grade"):
-            conditions.append("c.grade = %(grade)s")
-
-    return " AND ".join(conditions) if conditions else ""
+def get_number_of_boes(container_name, cone):
+    con = frappe.get_doc("Container", container_name)
+    return len([batch for batch in con.batches if batch.cone == cone])
