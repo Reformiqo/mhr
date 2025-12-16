3edcb3d6728241e8d903c49e9a6f951ed0361fe6
khanreformiqo <momdou.khan@reformiqo.com>
2025-12-16 04:22:17 +0000

    stock ledger doctore

--- mhr/mhr/report/stock_sheets_(inward_rest_stock_)/stock_sheets_(inward_rest_stock_).js
+++ mhr/mhr/report/stock_sheets_(inward_rest_stock_)/stock_sheets_(inward_rest_stock_).js
@@ -3,6 +3,19 @@
 
 frappe.query_reports["STOCK SHEETS (INWARD REST STOCK )"] = {
 	"filters": [
-
+		{
+			"fieldname": "fdt",
+			"fieldtype": "Date",
+			"label": "From Date",
+			"mandatory": 1,
+			"wildcard_filter": 0
+		},
+		{
+			"fieldname": "tdt",
+			"fieldtype": "Date",
+			"label": "To Date",
+			"mandatory": 1,
+			"wildcard_filter": 0
+		}
 	]
 };
