40dbb5f1c244a50856f2014c0184ec8fa5d8c961
khanreformiqo <momdou.khan@reformiqo.com>
2025-12-26 19:28:29 +0000

    update

--- mhr/mhr/report/stock_sheet_(inward_cone_wise)/stock_sheet_(inward_cone_wise).py
+++ mhr/mhr/report/stock_sheet_(inward_cone_wise)/stock_sheet_(inward_cone_wise).py
@@ -73,104 +73,78 @@ def get_data(filters=None):
     if not fdt or not tdt:
         frappe.throw(_("Please select From Date and To Date"))
 
-    # Build WHERE conditions dynamically
-    where_conditions = ["c.posting_date BETWEEN %(fdt)s AND %(tdt)s", "c.docstatus != 2"]
+    # Build WHERE conditions dynamically for Batch table
+    where_conditions = ["b.creation BETWEEN %(fdt)s AND %(tdt)s"]
 
     if container:
-        where_conditions.append("c.container_no = %(container)s")
+        where_conditions.append("b.custom_container_no = %(container)s")
 
     if lot_no:
-        where_conditions.append("c.lot_no = %(lot_no)s")
+        where_conditions.append("b.custom_lot_no = %(lot_no)s")
 
     if cone:
-        where_conditions.append("bi.cone = %(cone)s")
+        where_conditions.append("b.custom_cone = %(cone)s")
 
     where_clause = " AND ".join(where_conditions)
 
+    # Use Stock Ledger Entry for accurate IN/OUT qty tracking
+    # IN qty = positive actual_qty from SLE (Purchase Receipt inward)
+    # OUT qty = negative actual_qty from SLE (Delivery Note outward)
     query = f"""
-		WITH main_data AS (
+		WITH batch_data AS (
 			SELECT
-				DATE_FORMAT(c.posting_date, '%%d/%%m/%%Y') AS report_date,
-				c.container_no AS container_no,
-				bi.item AS item,
-				c.pulp AS pulp,
-				c.lusture AS lusture,
-				c.glue AS glue,
-				c.grade AS grade,
-				ROUND(
-					SUM(CAST(bi.qty AS DECIMAL(18,2))) +
-					COALESCE((
-						SELECT SUM(dni2.qty)
-						FROM `tabDelivery Note Item` dni2
-						INNER JOIN `tabDelivery Note` dn2 ON dn2.name = dni2.parent
-						WHERE dni2.batch_no = bi.batch_id
-						AND dn2.docstatus = 1
-						AND dn2.is_return = 1
-					), 0),
-				2) AS in_qty,
-				ROUND(
-					COALESCE((
-						SELECT SUM(dni3.qty)
-						FROM `tabDelivery Note Item` dni3
-						INNER JOIN `tabDelivery Note` dn3 ON dn3.name = dni3.parent
-						WHERE dni3.batch_no = bi.batch_id
-						AND dn3.docstatus = 1
-						AND COALESCE(dn3.is_return, 0) = 0
-					), 0) +
-					COALESCE((
-						SELECT SUM(pri2.qty)
-						FROM `tabPurchase Receipt Item` pri2
-						INNER JOIN `tabPurchase Receipt` pr2 ON pr2.name = pri2.parent
-						WHERE pri2.batch_no = bi.batch_id
-						AND pr2.docstatus = 1
-						AND pr2.is_return = 1
-					), 0),
-				2) AS out_qty,
-				ROUND(
-					(SUM(CAST(bi.qty AS DECIMAL(18,2))) +
-					COALESCE((
-						SELECT SUM(dni2.qty)
-						FROM `tabDelivery Note Item` dni2
-						INNER JOIN `tabDelivery Note` dn2 ON dn2.name = dni2.parent
-						WHERE dni2.batch_no = bi.batch_id
-						AND dn2.docstatus = 1
-						AND dn2.is_return = 1
-					), 0)) -
-					(COALESCE((
-						SELECT SUM(dni3.qty)
-						FROM `tabDelivery Note Item` dni3
-						INNER JOIN `tabDelivery Note` dn3 ON dn3.name = dni3.parent
-						WHERE dni3.batch_no = bi.batch_id
-						AND dn3.docstatus = 1
-						AND COALESCE(dn3.is_return, 0) = 0
-					), 0) +
-					COALESCE((
-						SELECT SUM(pri2.qty)
-						FROM `tabPurchase Receipt Item` pri2
-						INNER JOIN `tabPurchase Receipt` pr2 ON pr2.name = pri2.parent
-						WHERE pri2.batch_no = bi.batch_id
-						AND pr2.docstatus = 1
-						AND pr2.is_return = 1
-					), 0)),
-				2) AS stock,
-				c.lot_no AS lot_no,
-				COUNT(DISTINCT bi.batch_id) AS total_box,
-				bi.cone AS cone,
+				b.name AS batch_id,
+				b.item AS item,
+				b.custom_container_no AS container_no,
+				b.custom_lot_no AS lot_no,
+				b.custom_cone AS cone,
+				b.custom_pulp AS pulp,
+				b.custom_lusture AS lusture,
+				b.custom_glue AS glue,
+				b.custom_grade AS grade,
+				DATE(b.creation) AS batch_date
+			FROM `tabBatch` b
+			WHERE {where_clause}
+		),
+		sle_data AS (
+			SELECT
+				COALESCE(sle.batch_no, sbe.batch_no) AS batch_no,
+				SUM(CASE WHEN sle.actual_qty > 0 THEN sle.actual_qty ELSE 0 END) AS in_qty,
+				SUM(CASE WHEN sle.actual_qty < 0 THEN ABS(sle.actual_qty) ELSE 0 END) AS out_qty
+			FROM `tabStock Ledger Entry` sle
+			LEFT JOIN `tabSerial and Batch Entry` sbe ON sbe.parent = sle.serial_and_batch_bundle
+			WHERE sle.docstatus = 1
+			AND sle.is_cancelled = 0
+			GROUP BY COALESCE(sle.batch_no, sbe.batch_no)
+		),
+		main_data AS (
+			SELECT
+				DATE_FORMAT(bd.batch_date, '%%d/%%m/%%Y') AS report_date,
+				bd.container_no AS container_no,
+				bd.item AS item,
+				bd.pulp AS pulp,
+				bd.lusture AS lusture,
+				bd.glue AS glue,
+				bd.grade AS grade,
+				ROUND(COALESCE(sd.in_qty, 0), 2) AS in_qty,
+				ROUND(COALESCE(sd.out_qty, 0), 2) AS out_qty,
+				ROUND(COALESCE(sd.in_qty, 0) - COALESCE(sd.out_qty, 0), 2) AS stock,
+				bd.lot_no AS lot_no,
+				COUNT(DISTINCT bd.batch_id) AS total_box,
+				bd.cone AS cone,
 				0 AS sort_order
-			FROM `tabContainer` c
-			INNER JOIN `tabBatch Items` bi ON bi.parent = c.name
-			WHERE
-				{where_clause}
+			FROM batch_data bd
+			LEFT JOIN sle_data sd ON sd.batch_no = bd.batch_id
 			GROUP BY
-				c.posting_date,
-				c.container_no,
-				c.lot_no,
-				bi.cone,
-				bi.item,
-				c.pulp,
-				c.lusture,
-				c.glue,
-				c.grade
+				bd.batch_date,
+				bd.container_no,
+				bd.lot_no,
+				bd.cone,
+				bd.item,
+				bd.pulp,
+				bd.lusture,
+				bd.glue,
+				bd.grade
 		),
 		lot_total AS (
 			SELECT
